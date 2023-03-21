56945c02071540ae15a3ce782781890aa8e36c71
Akinola <akinolatofunmi04@gmail.com>
2023-03-21 03:58:41 +0000

    Fix all ESlint checks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,11 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable--next-line no-unused-vars
-    const task = true;
-    // eslint-disable--next-line no-unused-vars
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
-
   return [task, task2];
 }
