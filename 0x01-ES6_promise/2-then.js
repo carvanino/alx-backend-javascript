186e55a8ebbed3b14485241b252a9687cd966d3c
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 16:43:17 +0000

    dump

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,17 @@
 export default function handleResponseFromAPI(promise) {
   const res = { status: 200, body: 'success' };
-  const rej = new Error('');
-  promise.then(() => {return res})
-    .catch(() => {return rej})
-    .then(() => { console.log('Got a response from the API'); });
+  const rej = new Error('API request failed');
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return res;
+    })
+    .catch((error) => {
+      console.error('API request failed:', error);
+      return rej;
+    })
+    .then((result) => {
+      // this will only execute if the Promise resolved successfully
+      return result;
+    });
 }
