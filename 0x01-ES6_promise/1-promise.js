0f48b0b6f81b28f550181c25de10c9f2d20ea421
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 15:31:45 +0000

    Task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const promiseObject = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working correctly'));
+    }
+  });
+  return promiseObject;
+}
