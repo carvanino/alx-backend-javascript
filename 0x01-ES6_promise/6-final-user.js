cbe1597a92f7884671d9c6eb42d48a43a69026b5
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 20:15:43 +0000

    Remove log

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((values) => {
-      console.log(values);
+       return values;
     });
 }
