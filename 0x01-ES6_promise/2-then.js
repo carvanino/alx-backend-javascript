fcf2c24faaba9f576f3d74dcf63b048b793db5cc
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 17:03:25 +0000

    Finally()

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,13 +2,9 @@ export default function handleResponseFromAPI(promise) {
   const res = { status: 200, body: 'success' };
   const rej = new Error();
   promise
-    .then(() => {
+    .then(() => res)
+    .catch(() => rej)
+    .finally(() => {
       console.log('Got a response from the API');
-      return res;
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return rej;
-    })
-    .then((result) => result);
+    });
 }
