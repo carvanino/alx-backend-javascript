0602ed9b6b75d37728db75f624b6a44aefe991cf
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 16:02:16 +0000

    Fix handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   const res = { status: 200, body: 'success' };
   const rej = { error: '' };
-  promise.then((res, rej) => { res || rej; })
-    .then((res, rej) => { res || rej; })
-    .then((res, rej) => { console.log('Got a response from the API'); });
+  promise.then(() => {return res})
+    .catch(() => (Error ('')))
+    .then(() => { console.log('Got a response from the API') })
 }
