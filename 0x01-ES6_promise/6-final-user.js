c83d6c41851cd43fff13796070e444c1fe7e41be
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 21:48:01 +0000

    Return a seperature object from the return of allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,17 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((values) => {
-       return values;
+      const arr = [];
+      for (const value of values) {
+        const obj = {};
+        obj.status = value.status;
+        if (value.value === undefined) {
+          obj.value = value.reason;
+        } else {
+          obj.value = value.value;
+        }
+        arr.push(obj);
+      }
+      // console.log(arr)
     });
 }
