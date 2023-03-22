c2d492f4b80ac63e657f0cfcfe8bd2568b75474a
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 23:53:11 +0000

    Test map

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,18 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      const arr = [];
-      for (const value of values) {
-        const obj = {};
-        obj.status = value.status;
-        if (value.value === undefined) {
-          obj.value = value.reason;
-        } else {
-          obj.value = value.value;
-        }
-        arr.push(obj);
-      }
-      return arr
-    });
+    .then((res) => res.map((obj) => {
+            //console.log(obj);
+            return obj;
+    }));
 }
