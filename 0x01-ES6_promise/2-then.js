43a247880aee528da2c20b28c1907afefceea26a
Akinola <akinolatofunmi04@gmail.com>
2023-03-22 17:11:22 +0000

    Does the third handler have to be *finally*

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,7 +4,7 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => res)
     .catch(() => rej)
-    .finally(() => {
+    .then(() => {
       console.log('Got a response from the API');
     });
 }
