433abcb8b48b1391fd8d7a3f62832a8f9cb08825
Akinola <akinolatofunmi04@gmail.com>
2023-03-24 00:35:46 +0000

    Task 5 - Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqrt) {
+    this._sqrt = sqrt;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqrt() {
+    return this._sqrt;
+  }
+
+  set sqrt(value) {
+    this._sqrt = value;
+  }
+}
