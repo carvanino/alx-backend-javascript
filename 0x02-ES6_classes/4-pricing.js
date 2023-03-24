364a640c3e5ace3d9d3354e175d88e415197db89
Akinola <akinolatofunmi04@gmail.com>
2023-03-24 21:47:21 +0000

    Fix another typo

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -29,7 +29,7 @@ export default class Pricing {
     return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
 
-  static covertPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
