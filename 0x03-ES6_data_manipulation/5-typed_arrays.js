8e2580c2e624a41dcddc978b832217039f77b492
Akinola <akinolatofunmi04@gmail.com>
2023-03-27 23:10:31 +0000

    Include error condition

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
+  if (position >= length) {
+    throw new Error('Posiiton outside range');
+  }
   view.setInt8(position, value);
   // const typedArray = new Int8Array(buffer)
   // typedArray[position] = value
