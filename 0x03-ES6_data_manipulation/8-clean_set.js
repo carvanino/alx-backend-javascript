4e6c5e6a0e4c5ea72870c42cd4f4fbe92f6e56f5
Akinola <akinolatofunmi04@gmail.com>
2023-03-28 00:39:16 +0000

    Catching errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set) {
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   const newStr = [];
