02cfea8057dd109d73a91b205ba50eba48fb63b8
Akinola <akinolatofunmi04@gmail.com>
2023-03-28 00:26:40 +0000

    Fix conditional statements

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || !set) {
     return '';
   }
   const newStr = [];
   set.forEach((values) => {
     if (values.startsWith(startString)) {
       const string = values.substring(startString.length);
-      newStr.push(string);
+      if (string) {
+      	newStr.push(string);
+      }
     }
   });
   return newStr.join('-');
