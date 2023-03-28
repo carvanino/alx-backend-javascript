1360fe51cf2dbbdf1d3ce14224e1e2c45b8f5069
Akinola <akinolatofunmi04@gmail.com>
2023-03-28 13:34:57 +0000

    Fix conditionals

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,10 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const count = weakMap.get(endpoint);
   weakMap.set(endpoint, count + 1);
   if (count >= 5) {
     throw new Error('Endpoint load is high');
