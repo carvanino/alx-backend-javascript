1fe1a4f01637987c33333681e69533b1a970d6c6
Akinola <akinolatofunmi04@gmail.com>
2023-06-11 10:19:37 +0100

    Task 2 - Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,31 +3,32 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-	  	lines = data.split('\n');
-	  	rows = lines.slice(1, -1);
-	  	// console.log(rows);
-	  	totalStudent = rows.length;
-	  	fields = {};
-	  	for (row of rows) {
-		  	studentInfo = row.split(',');
-		  	field = studentInfo.slice(-1);
-		  	// console.log(field)
-		  	if (!(field[0] in fields) & field[0] !== '') {
+    const lines = data.split('\n');
+    const rows = lines.slice(1, -1);
+    // console.log(rows);
+    const totalStudent = rows.length;
+    const fields = {};
+    for (const row of rows) {
+      const studentInfo = row.split(',');
+      const field = studentInfo.slice(-1);
+      // console.log(field)
+      if (!(field[0] in fields) && field[0] !== '') {
         fields[field[0]] = {};
-			  	fields[field[0]].count = 1;
-			  	fields[field[0]].firstName = [];
-			  	fields[field[0]].firstName.push(studentInfo[0]);
-		  	} else if (field[0] !== '') {
-			  	fields[field[0]].count += 1;
-			  	fields[field[0]].firstName.push(studentInfo[0]);
-		  	}
-	  	}
-	  	console.log(`Number of students: ${totalStudent}`);
-	  	for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].firstName.join(', ')}`);
+        fields[field[0]].count = 1;
+        fields[field[0]].firstName = [];
+        fields[field[0]].firstName.push(studentInfo[0]);
+      } else if (field[0] !== '') {
+        fields[field[0]].count += 1;
+        fields[field[0]].firstName.push(studentInfo[0]);
+      }
     }
-	  	// console.log(fields)
-  	} catch (err) {
+    console.log(`Number of students: ${totalStudent}`);
+    for (const [field, value] of Object.entries(fields)) {
+      const list = value.firstName.join(', ');
+      console.log(`Number of students in ${field}: ${fields[field].count}. List: ${list}`);
+    }
+    // console.log(fields)
+  } catch (err) {
     console.error(err);
     throw new Error('Cannot load the database');
   }
