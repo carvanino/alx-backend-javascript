c6e6a2fbca1d2d3d4b4cff5346b6f59b79e88153
Akinola <akinolatofunmi04@gmail.com>
2023-06-11 12:10:08 +0100

    Export the right thing

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,15 @@
-const app = require('http');
+const http = require('http');
 
 const hostname = 'localhost';
 const port = 1245;
 
-const server = app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 module.exports = app;
