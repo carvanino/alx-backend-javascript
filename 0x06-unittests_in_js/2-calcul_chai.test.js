d09c1823cb9238e5ed709b648fd4a0bbf47edb0e
Akinola <akinolatofunmi04@gmail.com>
2023-06-14 14:43:46 +0100

    ..

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,23 +1,23 @@
 // const assert = require('assert');
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  describe('sUM', () => {
+  describe('SUM', () => {
     it('should SUM two numbers', () => {
-      expect(calculateNumber('SUM', 5.3, 4.5)).to.equal(10)
-      expect(calculateNumber('SUM', 5.0, 4.5)).to.equal(10)
-      expect(calculateNumber('SUM', -5.5, 4.5)).to.equal(0)
-      expect(calculateNumber('SUM', 5.0, -4.5)).to.equal(1)
-      expect(calculateNumber('SUM', 5.0, -4.6)).to.equal(0)
+      expect(calculateNumber('SUM', 5.3, 4.5)).to.equal(10);
+      expect(calculateNumber('SUM', 5.0, 4.5)).to.equal(10);
+      expect(calculateNumber('SUM', -5.5, 4.5)).to.equal(0);
+      expect(calculateNumber('SUM', 5.0, -4.5)).to.equal(1);
+      expect(calculateNumber('SUM', 5.0, -4.6)).to.equal(0);
     });
 
     it('should return NaN', () => {
       expect(isNaN(calculateNumber('SUM', 'a', 'b'))).to.equal(true);
       expect(isNaN(calculateNumber('SUM', [2, 4, 5], 5))).to.equal(true);
       expect(isNaN(calculateNumber('SUM'))).to.equal(true);
-      expect(isNaN(calculateNumber('SUM', 2.5))).to.equal(true)
-      expect(isNaN(calculateNumber())).to.equal(true)
+      expect(isNaN(calculateNumber('SUM', 2.5))).to.equal(true);
+      expect(isNaN(calculateNumber())).to.equal(true);
     });
   });
   describe('SUBTACT', () => {
