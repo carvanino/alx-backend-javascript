ef2373b25dfde0f1a7458c73b9d4694f43d4117b
Akinola <akinolatofunmi04@gmail.com>
2023-06-14 13:18:51 +0100

    Task 1 - Combining describtion

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('sUM', () => {
+    it('should SUM two numbers', () => {
+      assert.equal(calculateNumber('SUM', 5.3, 4.5), 10);
+      assert.equal(calculateNumber('SUM', 5.0, 4.5), 10);
+      assert.equal(calculateNumber('SUM', -5.5, 4.5), 0);
+      assert.equal(calculateNumber('SUM', 5.0, -4.5), 1);
+      assert.equal(calculateNumber('SUM', 5.0, -4.6), 0);
+    });
+
+    it('should return NaN', () => {
+      assert.ok(isNaN(calculateNumber('SUM', 'a', 'b'), NaN));
+      assert.ok(isNaN(calculateNumber('SUM', [2, 4, 5], 5)));
+      assert.ok(isNaN(calculateNumber('SUM')));
+      assert.ok(isNaN(calculateNumber('SUM', 2.5)));
+      assert.ok(isNaN(calculateNumber()));
+    });
+  });
+  describe('sUBTACT', () => {
+    it('should SUBTRACT two numbers', () => {
+      assert.equal(calculateNumber('SUBTRACT', 5.3, 4.5), 0);
+      assert.equal(calculateNumber('SUBTRACT', 5.0, -4.5), 9);
+      assert.equal(calculateNumber('SUBTRACT', -5.5, 4.5), -10);
+      assert.equal(calculateNumber('SUBTRACT', 5.0, -4.5), 9);
+      assert.equal(calculateNumber('SUBTRACT', 5.0, -4.6), 10);
+    });
+    it('should return NaN', () => {
+      assert.ok(isNaN(calculateNumber('SUBTRACT', 'a', 'b'), NaN));
+      assert.ok(isNaN(calculateNumber('SUBTRACT', [2, 4, 5], 5)));
+      assert.ok(isNaN(calculateNumber('SUBTRACT')));
+    });
+  });
+  describe('dIVIDE', () => {
+    it('should DIVIDE two numbers', () => {
+      assert.equal(calculateNumber('DIVIDE', 15.3, 4.5), 3);
+      assert.equal(calculateNumber('DIVIDE', 15.0, -4.5), -3.75);
+      assert.equal(calculateNumber('DIVIDE', -15.5, 4.5), -3);
+      assert.equal(calculateNumber('DIVIDE', 15.0, -4.5), -3.75);
+      assert.equal(calculateNumber('DIVIDE', 15.0, 0), 'Error');
+      assert.equal(calculateNumber('DIVIDE', 0.4, 12.0), 0);
+    });
+    it('should return NaN', () => {
+      assert.ok(isNaN(calculateNumber('DIVIDE', 'a', 'b'), NaN));
+      assert.ok(isNaN(calculateNumber('DIVIDE', [2, 4, 5], 5)));
+      assert.ok(isNaN(calculateNumber('DIVIDE')));
+    });
+  });
+});
