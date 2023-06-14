9d6851f3accaa4a8bade8ae072297eff24070b42
Akinola <akinolatofunmi04@gmail.com>
2023-06-14 21:23:39 +0100

    Remove double describe

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -20,33 +20,35 @@ describe('calculateNumber', () => {
       expect(isNaN(calculateNumber())).to.be.true;
     });
   });
-  describe('SUBTACT', () => {
-    it('should SUBTRACT two numbers', () => {
-      expect(calculateNumber('SUBTRACT', 5.3, 4.5)).to.equal(0);
-      expect(calculateNumber('SUBTRACT', 5.0, -4.5)).to.equal(9);
-      expect(calculateNumber('SUBTRACT', -5.5, 4.5)).to.equal(-10);
-      expect(calculateNumber('SUBTRACT', 5.0, -4.5)).to.equal(9);
-      expect(calculateNumber('SUBTRACT', 5.0, -4.6)).to.equal(10);
-    });
-    it('should return NaN', () => {
-      expect(isNaN(calculateNumber('SUBTRACT', 'a', 'b'))).to.be.true;
-      expect(isNaN(calculateNumber('SUBTRACT', [2, 4, 5], 5))).to.be.true;
-      expect(isNaN(calculateNumber('SUBTRACT'))).to.be.true;
-    });
+});
+
+describe('SUBTACT', () => {
+  it('should SUBTRACT two numbers', () => {
+    expect(calculateNumber('SUBTRACT', 5.3, 4.5)).to.equal(0);
+    expect(calculateNumber('SUBTRACT', 5.0, -4.5)).to.equal(9);
+    expect(calculateNumber('SUBTRACT', -5.5, 4.5)).to.equal(-10);
+    expect(calculateNumber('SUBTRACT', 5.0, -4.5)).to.equal(9);
+    expect(calculateNumber('SUBTRACT', 5.0, -4.6)).to.equal(10);
   });
-  describe('DIVIDE', () => {
-    it('should DIVIDE two numbers', () => {
-      expect(calculateNumber('DIVIDE', 15.3, 4.5)).to.equal(3);
-      expect(calculateNumber('DIVIDE', 15.0, -4.5)).to.equal(-3.75);
-      expect(calculateNumber('DIVIDE', -15.5, 4.5)).to.equal(-3);
-      expect(calculateNumber('DIVIDE', 15.0, -4.5)).to.equal(-3.75);
-      expect(calculateNumber('DIVIDE', 15.0, 0)).to.equal('Error');
-      expect(calculateNumber('DIVIDE', 0.4, 12.0)).to.equal(0);
-    });
-    it('should return NaN', () => {
-      expect(isNaN(calculateNumber('DIVIDE'))).to.be.true;
-      expect(isNaN(calculateNumber('DIVIDE', 'a', 'b', NaN))).to.be.true;
-      expect(isNaN(calculateNumber('DIVIDE', [2, 4, 5], 5))).to.be.true;
-    });
+  it('should return NaN', () => {
+    expect(isNaN(calculateNumber('SUBTRACT', 'a', 'b'))).to.be.true;
+    expect(isNaN(calculateNumber('SUBTRACT', [2, 4, 5], 5))).to.be.true;
+    expect(isNaN(calculateNumber('SUBTRACT'))).to.be.true;
   });
 });
+describe('DIVIDE', () => {
+  it('should DIVIDE two numbers', () => {
+    expect(calculateNumber('DIVIDE', 15.3, 4.5)).to.equal(3);
+    expect(calculateNumber('DIVIDE', 15.0, -4.5)).to.equal(-3.75);
+    expect(calculateNumber('DIVIDE', -15.5, 4.5)).to.equal(-3);
+    expect(calculateNumber('DIVIDE', 15.0, -4.5)).to.equal(-3.75);
+    expect(calculateNumber('DIVIDE', 15.0, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 0.4, 12.0)).to.equal(0);
+  });
+  it('should return NaN', () => {
+    expect(isNaN(calculateNumber('DIVIDE'))).to.be.true;
+    expect(isNaN(calculateNumber('DIVIDE', 'a', 'b', NaN))).to.be.true;
+    expect(isNaN(calculateNumber('DIVIDE', [2, 4, 5], 5))).to.be.true;
+  });
+});
+
