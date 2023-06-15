a01965f17733ef3f96a64b0b626ff2175d815dea
Akinola <akinolatofunmi04@gmail.com>
2023-06-15 13:44:09 +0100

    Add test for body

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,9 +2,10 @@ const request = require('request');
 const assert = require('assert');
 
 describe('Test the Index page', () => {
-  it('should check for correct status code', (done) => {
-    request.get('http://localhost:7865', (error, response) => {
+  it('should check for correct status code and response body', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
       assert.equal(response.statusCode, 200);
+      assert.equal(body, 'Welcome to the payment system')
       done();
     });
   });
